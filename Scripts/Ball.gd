dc696b8c99620c77c30edf59f4c22807484b91ad
Thilo Mothes <tmothes@gmail.com>
2022-04-09 14:14:43 +0200

    game gets faster over time

--- Scripts/Ball.gd
+++ Scripts/Ball.gd
@@ -1,8 +1,9 @@
 extends KinematicBody2D
 
 class_name Ball
+signal check_game_status
 
-export(int) var speed = 150
+export(int) var speed = 111
 
 var rng = RandomNumberGenerator.new()
 var velocity: Vector2 = Vector2.ZERO
@@ -10,10 +11,14 @@ var velocity: Vector2 = Vector2.ZERO
 func _ready():
 	rng.randomize()
 	velocity = Vector2(rng.randf_range(-1, 1), -1).normalized()
+	speed = speed + (275 - get_tree().get_nodes_in_group("Blocks").size()) * 0.25
 
 func _physics_process(delta):
+	print(speed)
 	var collision = move_and_collide(velocity.normalized() * speed * delta)
 	if collision:
 		velocity = velocity.bounce(collision.normal)
 		if collision.collider is Block:
+			speed += 0.25
 			collision.collider.queue_free()
+			emit_signal("check_game_status")
--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -1,12 +1,11 @@
 extends Node2D
 
-
-export(PackedScene) var ball_scene
+onready var ball_scene = preload("res://Scenes/Ball.tscn")
 
 var ball
 
 func _ready():
-	pass# instantiate_ball()
+	pass
 
 func _process(delta):
 	if Input.get_action_strength("start_game") and !ball:
@@ -14,6 +13,7 @@ func _process(delta):
 		
 func instantiate_ball():
 	ball = ball_scene.instance()
+	ball.connect("check_game_status", self, "_on_check_game_status")
 	add_child(ball)
 	ball.global_position = $Player/BallStartPosition.global_position
 
@@ -21,7 +21,18 @@ func _on_DamageArea_body_entered(body):
 	if body is Ball:
 		body.queue_free()
 		ball = null
+		$BlackScreen.visible = true
+		yield(get_tree().create_timer(0.05), "timeout")
+		$BlackScreen.visible = false
 		$Player.decrease_health()
 
 func _on_Player_game_over():
 	get_tree().reload_current_scene()
+
+func _on_check_game_status():
+	var blocks = get_tree().get_nodes_in_group("Blocks").size()
+	if blocks == 0:
+		end_game()
+
+func end_game():
+	pass
